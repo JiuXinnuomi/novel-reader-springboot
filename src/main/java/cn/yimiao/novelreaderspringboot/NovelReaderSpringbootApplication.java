94ca4a175d36c356b55f13caffb1d5b7471919dd
JiuXinnuomi <knight9455@foxmail.com>
2021-10-19 16:03:24 +0800

    init product

---
+++ src/main/java/cn/yimiao/novelreaderspringboot/NovelReaderSpringbootApplication.java
@@ -0,0 +1,13 @@
+package cn.yimiao.novelreaderspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelReaderSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelReaderSpringbootApplication.class, args);
+    }
+
+}
